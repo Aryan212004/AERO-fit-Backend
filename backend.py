14b406cee7ae891e25884b68beb0438bd16c261e
Aryan Gerathi <aryangerathi@Aryan-MacBook-Air.local>
2026-06-12 02:52:14 +0530

    uknowfbirwbr

--- backend.py
+++ backend.py
@@ -1,10 +1,11 @@
 # Patch missing pkg_resources for razorpay on Render
 try:
-    import pkg_resources
+    from pkg_resources import DistributionNotFound
 except ImportError:
     import types, sys
     pkg_resources = types.ModuleType("pkg_resources")
     pkg_resources.get_distribution = lambda x: type("D", (), {"version": "0.0.0"})()
+    pkg_resources.DistributionNotFound = Exception
     sys.modules["pkg_resources"] = pkg_resources
     
 import os, sys, json, re, bcrypt, uuid, base64, traceback, threading, time, asyncio
